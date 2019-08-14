7129ca1249c814a8dab78b2e3c5a2d458d63afce
Denys Rusov <rusovd@gmail.com>
2019-08-14 15:54:25 +0300

    Updates

---
+++ precache-manifest.0f667ff5ba5c5420110548b31d2a5597.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa40ee20d0aa4176e42d562174495b18",
+    "url": "/staff/index.html"
+  },
+  {
+    "revision": "3e287225478ca9b792a2",
+    "url": "/staff/static/css/2.86768bea.chunk.css"
+  },
+  {
+    "revision": "3e287225478ca9b792a2",
+    "url": "/staff/static/js/2.d164b2c2.chunk.js"
+  },
+  {
+    "revision": "7caaa5a63a3943f555ec",
+    "url": "/staff/static/js/main.dd890d28.chunk.js"
+  },
+  {
+    "revision": "b01662d6810cdf611f8c",
+    "url": "/staff/static/js/runtime~main.3ecf2f01.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/staff/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/staff/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/staff/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/staff/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/staff/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/staff/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/staff/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/staff/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/staff/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/staff/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
